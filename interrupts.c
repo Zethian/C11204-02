2880825e0c942a72a068fafe993ccbc37114ebc9
Marcus Persson <zethiane@gmail.com>
2018-12-05 10:10:29 +0100

    Update UART interrupt to handle several UART calls
    Updated so that full message comes through when it is received in
    several UART calls. Requires carriage return to be appended at end of
    message

--- interrupts.c
+++ interrupts.c
@@ -18,48 +18,38 @@ int *memadr;
 
 
 
- /* UART handler for RX from HVPS */
+/* UART handler for RX from HVPS */
 void uart1_rx_handler(mss_uart_instance_t * this_uart){
 	uint8_t rx_buff[16]="";
 	uint32_t rx_size;
-	uint8_t buffer[50]="";
-	static unsigned int writing = 0;
+	uint8_t output[18]="";
+	rx_size = MSS_UART_get_rx(this_uart, rx_buff, sizeof(rx_buff)); /* Get message from HVPS and send it on to computer terminal */
 
-	rx_size = MSS_UART_get_rx(this_uart, rx_buff, sizeof(rx_buff)); /* Get message from external and send it on to computer terminal */
-	if(writing == 0)
-		buffer[0] = '\0';
-	if(rx_buff[0] != 0x0d){
-		writing = 1;
-		strncat((char*)buffer, rx_buff, rx_size);
-	}
-	else {
-		writing = 0;
-		MSS_UART_polled_tx_string(&g_mss_uart0, buffer);
-	}
-	memset(rx_buff, 0, sizeof(rx_buff)); /* Clear buffer */
+	MSS_UART_polled_tx_string(&g_mss_uart0, rx_buff);
+	//processData(rx_buff); /* Process data for certain commands */
+	memset(rx_buff, '\0', sizeof(rx_buff)); /* Clear buffer */
 }
 
 /* UART handler for RX from external source */
 
 void uart0_rx_handler(mss_uart_instance_t * this_uart){
-	unsigned char rx_buff[10] ="";
+	unsigned char rx_buff[30] ="";
 	unsigned int rx_size;
-	uint8_t buffer[50]="";
-	static unsigned int writing = 0;
+	static uint8_t buffer[50]="";
+	unsigned char send[50]="";
 	/* Get commands from terminal on connected computer and send them on to external*/
 	rx_size = MSS_UART_get_rx(this_uart, rx_buff, sizeof(rx_buff));
-	if(writing == 0)
-		buffer[0] = '\0';
-	if(rx_buff[0] != 0x0d){
-		writing = 1;
+	if(rx_buff[rx_size-1] != '\r'){
 		strncat((char*)buffer, rx_buff, rx_size);
 	}
-	else{
-		writing = 0;
-		MSS_UART_polled_tx_string(&g_mss_uart1, rx_buff);
+	else {
+		strncat((char*)buffer, rx_buff, rx_size-1);
+		getarray(send, buffer);
+		MSS_UART_polled_tx_string(&g_mss_uart1, send);
+		memset(buffer, '\0', sizeof(buffer));
+		memset(send, '\0', sizeof(send));
 	}
-	/* Clear buffers */
-	memset(rx_buff, 0, sizeof(rx_buff));
+
 }
 
 
@@ -106,7 +96,7 @@ void init_uart(int* memory){
 
 	memadr=*memory;
 	MSS_TIM64_init(MSS_TIMER_PERIODIC_MODE);
-	MSS_TIM64_load_immediate(0xFFFFFFFF, 0xFFFFFFFF);
+	MSS_TIM64_load_immediate(0x00000000, 0xFFFFFFFF);
 	MSS_UART_init(gp_my_uart1, MSS_UART_38400_BAUD, MSS_UART_DATA_8_BITS | MSS_UART_EVEN_PARITY | MSS_UART_ONE_STOP_BIT);
 	MSS_UART_init(&g_mss_uart0, MSS_UART_38400_BAUD, MSS_UART_DATA_8_BITS | MSS_UART_EVEN_PARITY | MSS_UART_ONE_STOP_BIT);
 	MSS_UART_set_rx_handler(&g_mss_uart0, uart0_rx_handler, MSS_UART_FIFO_SINGLE_BYTE);
